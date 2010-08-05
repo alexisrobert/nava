62afe9004d157a7f1b25976a02ac00fd8628b14f
Alexis Robert <alexis.robert@gmail.com>
2010-08-05 02:27:47 +0200

    Add a script to help building nava scripts to native ELF executables

---
+++ build-script.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [[ ! -f "$1" ]] ; then
+	echo "Usage: $0 [source]"
+	exit 1
+fi
+
+TEMPLL=$(mktemp "/tmp/tmp.XXXX.ll")
+TEMPS=$(mktemp "/tmp/tmp.XXXX.s")
+OUTPUT=$(basename "$1" ".nv")
+
+STDLIB="CMakeFiles/nava.dir/stdlib/stdlib.cpp.o"
+
+if [[ ! -f ${STDLIB} ]] ; then
+	echo "${STDLIB} not found."
+	exit 1
+elif [[ ! -f "nava" ]] ; then
+	echo "./nava not found."
+	exit 1
+fi
+
+echo "Generating LLVM bytecode ..."
+(./nava < $1) >/dev/null 2>${TEMPLL}
+
+echo "Converting bytecode to native code ..."
+llc -o ${TEMPS} ${TEMPLL} && g++ -o ${OUTPUT} ${TEMPS} ${STDLIB} && strip -s ${OUTPUT} && echo "Build OK!"
+
+rm -f ${TEMPS} ${TEMPLL}
