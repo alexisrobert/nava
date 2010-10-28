0a026e58690e2aac5a73687e359f1ff34a2ca6cc
Alexis Robert <alexis.robert@gmail.com>
2010-10-28 19:04:46 +0200

    build-script.sh now manages absolute paths

--- build-script.sh
+++ build-script.sh
@@ -7,21 +7,21 @@ fi
 
 TEMPLL=$(mktemp "/tmp/tmp.XXXX.ll")
 TEMPS=$(mktemp "/tmp/tmp.XXXX.s")
-OUTPUT=$(basename "$1" ".nv")
+OUTPUT=$(dirname "$1")/$(basename "$1" ".nv")
 LLCFLAGS="-enable-unsafe-fp-math -O3 -time-passes -stats"
 
-STDLIB="CMakeFiles/nava.dir/stdlib/stdlib.cpp.o"
+STDLIB="$(dirname "$0")/CMakeFiles/nava.dir/stdlib/stdlib.cpp.o"
 
 if [[ ! -f ${STDLIB} ]] ; then
 	echo "${STDLIB} not found."
 	exit 1
-elif [[ ! -f "nava" ]] ; then
-	echo "./nava not found."
+elif [[ ! -f "$(dirname "$0")/nava" ]] ; then
+	echo "nava not found."
 	exit 1
 fi
 
 echo "Generating LLVM bytecode ..."
-(./nava < $1) >/dev/null 2>${TEMPLL}
+($(dirname "$0")/nava < $1) >/dev/null 2>${TEMPLL}
 
 echo "Converting bytecode to native code ..."
 llc ${LLCFLAGS} -o ${TEMPS} ${TEMPLL} && g++ -o ${OUTPUT} ${TEMPS} ${STDLIB} && strip -s ${OUTPUT} && echo "Build OK!"
